93ca85440138ad8ff4b150145b2c4babe6aa035a
Lavonathan <lavon_edwards@hotmail.com>
2021-12-05 13:01:04 -0600

    Added logo image and top navigation

--- config/routes.rb
+++ config/routes.rb
@@ -11,21 +11,21 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
 
-  # resources :products
-  get "/products", to: "products#index", as: "products" #products_path
-  get "/products/:id", to: "products#show", as: "product" #product_path
+  resources :products, only: [:index, :show]
+  # get "/products", to: "products#index", as: "products" #products_path
+  # get "/products/:id", to: "products#show", as: "product" #product_path
 
-  # resources :platforms
-  get "/platforms", to: "platforms#index", as: "platforms" #platforms_path
-  get "/platforms/:id", to: "platforms#show", as: "platform" #platform_path
+  resources :platforms, only: [:index, :show]
+  # get "/platforms", to: "platforms#index", as: "platforms" #platforms_path
+  # get "/platforms/:id", to: "platforms#show", as: "platform" #platform_path
 
-  # resources :genres
-  get "/genres", to: "genres#index", as: "genres" #genres_path
-  get "/genres/:id", to: "genres#show", as: "genre" #genre_path
+  resources :genres, only: [:index, :show]
+  # get "/genres", to: "genres#index", as: "genres" #genres_path
+  # get "/genres/:id", to: "genres#show", as: "genre" #genre_path
 
-  # resources :publishers
-  get "/publishers", to: "publishers#index", as: "publishers" #publishers_path
-  get "/publishers/:id", to: "publishers#show", as: "publisher" #publisher_path
+  resources :publishers, only: [:index, :show]
+  # get "/publishers", to: "publishers#index", as: "publishers" #publishers_path
+  # get "/publishers/:id", to: "publishers#show", as: "publisher" #publisher_path
 end
 
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
