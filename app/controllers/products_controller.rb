48a53954ebb5f6195876f24cc56b43aad4ecb5dc
Lavonathan <lavon_edwards@hotmail.com>
2021-12-05 11:22:55 -0600

    Added image to products show page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,5 +5,6 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.includes(:platforms, :publisher, :genres).find(params[:id])
+    @image = @product.image
   end
 end
