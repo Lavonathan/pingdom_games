1949095af9f15fb05d10b4a20a60cfdb571acb6f
Lavonathan <lavon_edwards@hotmail.com>
2021-12-05 14:08:47 -0600

    Improved menu navigation code.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  root to: 'products#index'
 
   resources :products, only: [:index, :show]
   # get "/products", to: "products#index", as: "products" #products_path
