daab37b52ac544260b2fb43490852432d48e8076
Lavonathan <lavon_edwards@hotmail.com>
2021-12-06 17:50:01 -0600

    More advanced searched functionality completed.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,9 +4,22 @@ class ProductsController < ApplicationController
 
     @wildcard_search = "%#{@search_value}%"
 
+    puts "good 1"
     # Search based on game name.
     begin
-      @product_total = Product.where("name LIKE ?", @wildcard_search)
+
+      if params[:platform] != nil && params[:platform] != "Console"
+        begin
+          @platform = Platform.includes(:products).find(params[:platform])
+          @product_total = @platform.products.where("name LIKE ?", @wildcard_search)
+        rescue
+          @platform = nil
+          @product_total = Product.where("name LIKE ?", @wildcard_search)
+        end
+      else
+        @platform = nil
+        @product_total = Product.where("name LIKE ?", @wildcard_search)
+      end
 
       # If there is only one pokemon returned, go right to their show page.
       if @product_total.count == 1
