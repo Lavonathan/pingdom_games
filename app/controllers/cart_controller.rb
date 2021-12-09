e33ba8c9ed4c34f5fa54a453cd7ba653f998c83e
Lavonathan <lavon_edwards@hotmail.com>
2021-12-09 15:36:25 -0600

    Cart addition and removal stable.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,12 +4,14 @@ class CartController < ApplicationController
   def create
     # the logger will show in the rails server.. super useful for debugging!!
     product_id = params[:id].to_i
+    quantity = params[:quantity].to_i
 
     logger.debug("Adding #{product_id} to cart.")
     product = Product.find(product_id)
+
     session[:shopping_cart] << product_id
 
-    flash[:notice] = "➕ #{product.name} added to cart."
+    flash[:notice] = "➕#{quantity} #{'copy'.pluralize(quantity)} of  #{product.name} added to cart."
 
     redirect_back(fallback_location: root_path)
   end
@@ -19,6 +21,7 @@ class CartController < ApplicationController
     # removes params[:id] from cart
     logger.debug("Removing #{params[:id]} from cart.")
     id = params[:id].to_i
+    puts "HI HI HI #{session[:shopping_cart]}"
     session[:shopping_cart].delete(id)
     product = Product.find(id)
     flash[:notice] = "➖ #{product.name} removed from cart."
